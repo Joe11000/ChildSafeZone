07600c665dae226d3801086735f3ce4affd5d75e
Joe Noonan <joenoonan27@gmail.com>
2014-10-25 15:03:15 -0500

    restraunt in naperville not in dekalb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # SafeZone.attr_accessible(:location_name, :person_name, :address, :latitude, :longitude)
 
-SafeZone.create(location_name: "BurritoVille", person_name: "Pizza Pizzaington", address:"1026 W Hillcrest Dr
-DeKalb, IL 60115", latitude: 41.9448800, longitude: -88.7720890)
+SafeZone.create(location_name: "BurritoVille", person_name: "Pizza Pizzaington", address: "1026 W Hillcrest Dr
+DeKalb, IL 60115", latitude: 41.8047990, longitude: -88.1257980)
+
 
